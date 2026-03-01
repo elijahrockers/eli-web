2cd530492fe40bb2916eed5b33986b27f06d987a
erockers3 <erockers3@gatech.edu>
2026-03-01 01:32:07 +0000

    fixing base path issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://elijahrockers.github.io',
+  base: '/eli-web/',
   vite: {
     plugins: [tailwindcss()]
   }
